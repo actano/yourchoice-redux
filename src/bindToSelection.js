0b1fad5565152950d5d5844ba1e3332f0b0a23c1
Marcus Mennemeier <marcus.mennemeier@actano.de>
2018-09-16 21:00:19 +0200

    Lodash: Replace mapValues with native implementation

--- src/bindToSelection.js
+++ src/bindToSelection.js
@@ -1,4 +1,4 @@
-import { flow, curry, get, mapValues } from 'lodash/fp'
+import { flow, curry, get } from 'lodash/fp'
 import { bind } from 'lodash'
 
 const _bindSelector = curry((selectionName, selector) =>
@@ -9,14 +9,15 @@ const _bindSelector = curry((selectionName, selector) =>
 )
 
 const bindToSelection = (actions, selectors) => (selectionName = 'selection') => {
-  const boundActions = mapValues(
-    actionCreator => bind(actionCreator, null, selectionName),
-    actions,
-  )
-  const boundSelectors = mapValues(
-    _bindSelector(selectionName),
-    selectors,
-  )
+  const boundActions = {}
+  const mapFn = actionCreator => bind(actionCreator, null, selectionName)
+  for (const key of Object.keys(actions)) {
+    boundActions[key] = mapFn(actions[key])
+  }
+  const boundSelectors = {}
+  for (const key of Object.keys(selectors)) {
+    boundSelectors[key] = _bindSelector(selectionName, selectors[key])
+  }
   return {
     actions: boundActions,
     selectors: boundSelectors,
